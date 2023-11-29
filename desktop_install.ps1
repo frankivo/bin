8bf34f6426a8ca95409d5852a14bcb9a08b2e7bc
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2023-11-29 12:09:10 +0100

    add desktop installer

---
+++ desktop_install.ps1
@@ -0,0 +1,23 @@
+# First upgrade winget using the Microsoft Store
+
+winget install -e --id AgileBits.1Password 
+
+winget install -e --id Microsoft.PowerShell
+winget install -e --id Python.Python.3.11
+winget install -e --id Git.Git
+winget install -e --id Axosoft.GitKraken
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id 7zip.7zip
+winget install --id=dotPDNLLC.paintdotnet  -e
+winget install --id=Microsoft.Azure.StorageExplorer  -e
+winget install -e --id Canonical.Ubuntu.2204 # wsl --install + reboot
+winget install -e --id Insomnia.Insomnia
+winget install -e --id Postman.Postman
+
+winget install -e --id Mozilla.Firefox
+winget install -e --id Mozilla.Thunderbird
+winget install -e --id Google.Chrome
+
+winget install -e --id OpenWhisperSystems.Signal
+winget install -e --id Spotify.Spotify
+winget install --id 9NKSQGP7F2NH --source msstore # WhatsApp Desktop
